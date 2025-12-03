d25c5ba6c0bb8ea7e186ffe65038cb73ebdce75d
Dumbrava Claudiu <claudiu_linz@yahoo.com>
2025-12-03 23:33:49 +0100

    added testimonials, details section and 2 Modals

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Add custom JavaScript here
+
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("bg-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("bg-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
